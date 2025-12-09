4b04b780a1a80922b220197d409745e4e70e7daa
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-12-09 07:03:39 +0000

    Made the app work with redux

--- frontend/src/store/employeesSlice.ts
+++ frontend/src/store/employeesSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { type Employees } from '../types/tables';
 import { initialEmployeesData } from '../data/mockData';
 
